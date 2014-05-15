739260cab72c0f9bdb3d9d6c33919d613aa933f3
Derek Wu <derek.x.wu@gmail.com>
2014-05-15 13:55:59 -0400

    First commit
    First commit of the script to Github

---
+++ hexchat.np.mpc-hc.py
@@ -0,0 +1,20 @@
+# Inspired by https://github.com/mpc-hc/snippets
+import urllib2
+import hexchat
+
+__module_name__ = 'MPC-HC Now Playing'
+__module_version__ = '0.2'
+__module_description__ = 'MPC-HC Now Playing script - Filename only'
+
+def now_playing(word, word_eol, userdata):
+	try:
+		page = urllib2.urlopen('http://127.0.0.1:13579/info.html').read()
+		line = page[page.find('&bull; ')+7:]
+		line = line[:line.find('&bull;')]
+		hexchat.command("SAY np: "+line)
+	except:
+		hexchat.prnt("Nothing open in MPC-HC")
+	return hexchat.EAT_ALL;
+
+hexchat.hook_command('np', now_playing, help='"/np" to display currently playing MPC-HC media')
+hexchat.prnt('mpc.hc.np.py loaded')
---
+++ readme.md
@@ -0,0 +1,6 @@
+#hexchat.np.mpc-hc.py
+###Installation
+Copy this into HexChat's `addons` folder.
+
+###More
+[MPC-HC](https://github.com/mpc-hc/snippets) has versions in mIRC, Perl, and Python.
