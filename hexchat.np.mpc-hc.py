8185296571621357161a1f3572319f3fc5f23c9f
Derek Wu <derek.x.wu@gmail.com>
2014-08-26 14:51:14 -0400

    Added full print
    Use regex instead of find to search
    Add option to print more than file name

--- hexchat.np.mpc-hc.py
+++ hexchat.np.mpc-hc.py
@@ -1,19 +1,26 @@
 # Inspired by https://github.com/mpc-hc/snippets
 import urllib2
 import hexchat
+import re
 
 __module_name__ = 'MPC-HC Now Playing'
-__module_version__ = '0.2'
-__module_description__ = 'MPC-HC Now Playing script - Filename only'
+__module_version__ = '0.3'
+__module_description__ = 'MPC-HC Now Playing script'
 
 def now_playing(word, word_eol, userdata):
 	try:
 		page = urllib2.urlopen('http://127.0.0.1:13579/info.html').read()
-		line = page[page.find('&bull; ')+7:]
-		line = line[:line.find('&bull;')]
-		hexchat.command("SAY np: "+line)
 	except:
-		hexchat.prnt("Nothing open in MPC-HC")
+		hexchat.prnt('Nothing open in MPC-HC')
+		return
+	line = re.search('&laquo;\s(.*?)\s&bull;\s(.*?)\s\&bull;\s(.*?)\s&bull;\s(.*?)\s&raquo;', page)
+	try:
+		if word[1] == 'v' or word[1] == 'full':
+			hexchat.command('SAY np: {1} @ {2}, {3} with {0}'.format(line.group(1), line.group(2), line.group(3), line.group(4)))
+		else:
+			hexchat.command('SAY np: '+line.group(2))
+	except IndexError:
+		hexchat.command('SAY np: '+line.group(2))
 	return hexchat.EAT_ALL;
 
 hexchat.hook_command('np', now_playing, help='"/np" to display currently playing MPC-HC media')
